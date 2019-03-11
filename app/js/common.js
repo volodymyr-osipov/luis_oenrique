2d4b623edc0c71fccb64bd9a3bed4f40df0d1441
volodymyr-osipov <volodymyr-os@ukr.net>
2019-03-11 19:05:44 +0200

    section testimonial, slick slider, spincrement

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
+//slick-clider
 $(function() {
+	$(".slider").slick ({
+		autoplay: false,
+		dots: true,
+		arrows: false,
+	});
+});
 
-	// Пользовательские функции
+//number
+$(document).ready(function(){
+var show = true;
+var countbox = ".testimonial";
+$(window).on("scroll load resize", function(){
+	if(!show) return false;                 
+	var w_top = $(window).scrollTop();    
+	var e_top = $(countbox).offset().top;   
+	var w_height = $(window).height();    
+	var d_height = $(document).height();    
+	var e_height = $(countbox).outerHeight(); 
 
+	if(w_top + 100 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+		$(".m__count").spincrement({
+			thousandSeparator: "",
+			duration: 1200
+			});
+			show = false;
+		}
+	});
 });
